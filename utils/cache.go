9d257e3ed191454aece90bead76e2a2403c873c2
Dexter Easter <dexter@easter.company>
2026-02-27 08:57:10 -0500

    12.33.41 -> 12.33.42
    Co-authored-by: Owen Easter <owen@easter.company>

--- utils/cache.go
+++ utils/cache.go
@@ -4,10 +4,8 @@ import (
 	"context"
 	"crypto/sha256"
 	"fmt"
-	"log"
 	"time"
 
-	"github.com/EasterCompany/dex-go-utils/network"
 	"github.com/redis/go-redis/v9"
 )
 
@@ -16,15 +14,6 @@ var RDB *redis.Client
 // DEPRECATED: Initialize RDB via network.NewBrain("web").Stem().Client() in main.go
 func GetRedisClient() *redis.Client {
 	return RDB
-}
-
-func UpdateWebViewState(ctx context.Context, rdb *redis.Client, targetURL string, field string, response string) error {
-	if rdb == nil {
-		return fmt.Errorf("redis not initialized")
-	}
-
-	key := fmt.Sprintf("web:cache:%x", sha256.Sum256([]byte(targetURL)))
-	return rdb.HSet(ctx, key, field, response).Err()
 }
 
 func GetWebViewCache(ctx context.Context, targetURL string) (string, error) {
